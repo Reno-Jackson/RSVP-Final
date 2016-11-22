dd7ac36fca3116e2a3af5cf5c59b5e2378b42672
Reno-Jackson <live.create.enjoy@gmail.com>
2016-11-22 15:49:20 -0700

    Updated Styling and Layout

--- public/js/main.js
+++ public/js/main.js
@@ -121,34 +121,6 @@ function yelpController($http, $routeProvider, $routeParams, $location) {
 
         return '';
     }
-
-    //////////////////////////////////////////////////////
-    //Functions for Owl-Carousel//////////////////////////
-    //////////////////////////////////////////////////////
-
-    // $('.center').carousel({
-    //     centerMode: true,
-    //     centerPadding: '60px',
-    //     slidesToShow: 3,
-    //     responsive: [{
-    //         breakpoint: 768,
-    //         settings: {
-    //             arrows: false,
-    //             centerMode: true,
-    //             centerPadding: '40px',
-    //             slidesToShow: 3
-    //         }
-    //     }, {
-    //         breakpoint: 480,
-    //         settings: {
-    //             arrows: false,
-    //             centerMode: true,
-    //             centerPadding: '40px',
-    //             slidesToShow: 1
-    //         }
-    //     }]
-    // });
-
     //////////////////////////////////////////////////////
     // Yelp Functions/////////////////////////////////////
     //////////////////////////////////////////////////////
